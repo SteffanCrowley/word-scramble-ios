4c6e3ba4c47f10a18e4a50259819ae7545010e18
Steffan Crowley <steffancrow@gmail.com>
2022-08-21 11:32:32 -0700

    Initial Commit

---
+++ word-scramble-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  word-scramble-ios
+//
+//  Created by steffan crowley on 8/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ word-scramble-ios/word_scramble_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  word_scramble_iosApp.swift
+//  word-scramble-ios
+//
+//  Created by steffan crowley on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct word_scramble_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
